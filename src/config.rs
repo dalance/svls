97e15b77d8642210149f2c5a7c86fc88561b0a9d
dalance <dalance@gmail.com>
2019-11-26 12:38:40 +0900

    Add file

---
+++ src/config.rs
@@ -0,0 +1,50 @@
+use serde_derive::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Config {
+    #[serde(default)]
+    pub option: ConfigOption,
+    #[serde(default)]
+    pub verilog: ConfigVerilog,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ConfigOption {
+    #[serde(default = "default_as_true")]
+    pub linter: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ConfigVerilog {
+    #[serde(default)]
+    pub include_paths: Vec<PathBuf>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        toml::from_str("").unwrap()
+    }
+}
+
+impl Default for ConfigOption {
+    fn default() -> Self {
+        toml::from_str("").unwrap()
+    }
+}
+
+impl Default for ConfigVerilog {
+    fn default() -> Self {
+        toml::from_str("").unwrap()
+    }
+}
+
+#[allow(dead_code)]
+fn default_as_true() -> bool {
+    true
+}
+
+#[allow(dead_code)]
+fn default_as_false() -> bool {
+    false
+}
